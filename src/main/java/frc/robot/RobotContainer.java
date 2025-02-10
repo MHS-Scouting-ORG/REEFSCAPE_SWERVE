8bd293b8d236b517255f17431e6dea3e105e9338
DylanJakeComilang <dylanjakecomilang@gmail.com>
2025-02-10 10:04:15 -1000

    added registerNamedCommands for path planner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,12 +3,14 @@ package frc.robot;
 import frc.robot.commands.DriveCommand;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 
@@ -22,6 +24,10 @@ public class RobotContainer {
   XboxController xboxController = new XboxController(0);
 
   public RobotContainer() {
+    
+    //register commands before putting data into auto chooser
+    registerNamedCommands();
+    
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Chooser", autoChooser);
 
@@ -31,6 +37,10 @@ public class RobotContainer {
 
   private void configureBindings() {
     new JoystickButton(xboxController, XboxController.Button.kA.value).onTrue(new InstantCommand(s_Subsystem::resetPigeon2));
+  }
+
+  private void registerNamedCommands(){
+    NamedCommands.registerCommand("Wait", new WaitCommand(5));
   }
 
  
